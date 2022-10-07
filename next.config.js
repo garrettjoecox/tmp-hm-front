079dfd94871d8cadcfd5d60bddcc1e1c17c9b4e9
Garrett Cox <garrettjcox@gmail.com>
2022-10-06 20:25:38 -0500

    User facing site

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -7,4 +8,13 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX(nextConfig);
